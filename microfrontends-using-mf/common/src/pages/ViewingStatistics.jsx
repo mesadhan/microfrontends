e843c5f8c5db005b405b3f916fd8bbace3e02bec
Sadhan Sarker <cse.sadhan@gmail.com>
2022-11-19 00:29:43 +0600

    replace class name to className and remove bold front

--- microfrontends-using-mf/common/src/pages/ViewingStatistics.jsx
+++ microfrontends-using-mf/common/src/pages/ViewingStatistics.jsx
@@ -16,11 +16,11 @@ const ViewingStatistics = () => {
     }
 
     // return <div class="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center bg-gray-100 font-sans py-6">
-    return <div class="min-w-screen min-h-screen flex justify-center bg-gray-100 font-sans py-6">
+    return <div className="min-w-screen min-h-screen flex justify-center bg-gray-100 font-sans py-6">
         <div className="container">
         
-            <div class="w-full py-3">
-                <div class="inline-block mr-2 mt-2">
+            <div className="w-full py-3">
+                <div className="inline-block mr-2 mt-2">
                 <Link className="focus:outline-none text-yellow-600 text-sm py-2.5 px-5 rounded-md border border-yellow-600 hover:bg-yellow-50 items-center mt-100" href="/" to={'/'}>Back Home</Link>               
                 </div>
             </div>
@@ -39,10 +39,10 @@ const ViewingStatistics = () => {
                 {visitedPagesStatistics.map((page, index) => (
 
                     <tr key={index} className="border border-grey-500 md:border-none block md:table-row">
-                        <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 font-bold">{page.url} </span></td>
-                        <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 font-bold">{page.counter} </span></td>
-                        <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 font-bold">{parseDate(page.created_at)} </span></td>
-                        <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3 font-bold">{parseDate(page.updated_at)} </span></td>
+                        <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3">{page.url} </span></td>
+                        <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3">{page.counter} </span></td>
+                        <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3">{parseDate(page.created_at)} </span></td>
+                        <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3">{parseDate(page.updated_at)} </span></td>
                     </tr>
 
                 ))}
