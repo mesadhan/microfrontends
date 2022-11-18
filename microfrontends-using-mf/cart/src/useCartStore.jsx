dc927866a974be2c7665fbc50f824a277b26865b
Sadhan Sarker <cse.sadhan@gmail.com>
2022-11-19 04:34:55 +0600

    cart list not persist issue resovled

--- microfrontends-using-mf/cart/src/useCartStore.jsx
+++ microfrontends-using-mf/cart/src/useCartStore.jsx
@@ -1,27 +1,32 @@
 import create from "zustand";
+import { persist } from 'zustand/middleware'
 
-const useCartStore = create((set) => ({
-  cart: [],
-  isShowCartPanel: true,
-  toggleCartPanel: () => set((state) => ({ isShowCartPanel: !state.isShowCartPanel })),
-  addToCartIfNotInCart: (product) => {
-    set((state) => {
-      
-      const cart = state.cart;
-      const index = cart.findIndex((p) => p.idDrink == product.idDrink);
-      if (index === -1) {
-        product['quantity'] = 1;
-        cart.push(product);
-      }
+const useCartStore = create(persist(
+  (set) => ({
+    cart: [],
+    isShowCartPanel: true,
+    toggleCartPanel: () => set((state) => ({ isShowCartPanel: !state.isShowCartPanel })),
+    addToCartIfNotInCart: (product) => {
+      set((state) => {
 
-      // increment quantity if product is already in cart
-      if (index !== -1) {
-        cart[index]['quantity'] = cart[index]['quantity'] + 1;
-      }
+        const cart = state.cart;
+        const index = cart.findIndex((p) => p.idDrink == product.idDrink);
+        if (index === -1) {
+          product['quantity'] = 1;
+          cart.push(product);
+        }
 
-      return { cart };
-    });
-  },
+        // increment quantity if product is already in cart
+        if (index !== -1) {
+          cart[index]['quantity'] = cart[index]['quantity'] + 1;
+        }
+
+        return { cart };
+      });
+    },
+  }), {
+  name: 'cart', // unique name
+  //getStorage: () => sessionStorage, // (optional) by default, 'localStorage' is used
 }));
 
 export default useCartStore;
