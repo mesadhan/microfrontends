41ea78183272da094e927e5f5eaa5ee72437bbe9
Sadhan Sarker <cse.sadhan@gmail.com>
2022-11-19 00:32:19 +0600

    replace gray to orange color

--- microfrontends-using-mf/common/src/pages/ViewingStatistics.jsx
+++ microfrontends-using-mf/common/src/pages/ViewingStatistics.jsx
@@ -21,28 +21,28 @@ const ViewingStatistics = () => {
         
             <div className="w-full py-3">
                 <div className="inline-block mr-2 mt-2">
-                <Link className="focus:outline-none text-yellow-600 text-sm py-2.5 px-5 rounded-md border border-yellow-600 hover:bg-yellow-50 items-center mt-100" href="/" to={'/'}>Back Home</Link>               
+                <Link className="focus:outline-none text-orange-600 text-sm py-2.5 px-5 rounded-md border border-orange-600 hover:bg-yellow-50 items-center mt-100" href="/" to={'/'}>Back Home</Link>               
                 </div>
             </div>
         
             <table className="min-w-full border-collapse block md:table">
             <thead className="block md:table-header-group">
                 <tr className="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
-                    <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Page</th>
-                    <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Visits</th>
-                    <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">First visited</th>
-                    <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Last visited</th>
+                    <th className="bg-orange-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Page</th>
+                    <th className="bg-orange-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Visits</th>
+                    <th className="bg-orange-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">First visited</th>
+                    <th className="bg-orange-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Last visited</th>
                 </tr>
             </thead>
             <tbody className="block md:table-row-group">
                 
                 {visitedPagesStatistics.map((page, index) => (
 
-                    <tr key={index} className="border border-grey-500 md:border-none block md:table-row">
-                        <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3">{page.url} </span></td>
-                        <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3">{page.counter} </span></td>
-                        <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3">{parseDate(page.created_at)} </span></td>
-                        <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span className="inline-block w-1/3">{parseDate(page.updated_at)} </span></td>
+                    <tr key={index} className="border border-orange-500 md:border-none block md:table-row">
+                        <td className="p-2 md:border md:border-orange-500 text-left block md:table-cell"><span className="inline-block w-1/3">{page.url} </span></td>
+                        <td className="p-2 md:border md:border-orange-500 text-left block md:table-cell"><span className="inline-block w-1/3">{page.counter} </span></td>
+                        <td className="p-2 md:border md:border-orange-500 text-left block md:table-cell"><span className="inline-block w-1/3">{parseDate(page.created_at)} </span></td>
+                        <td className="p-2 md:border md:border-orange-500 text-left block md:table-cell"><span className="inline-block w-1/3">{parseDate(page.updated_at)} </span></td>
                     </tr>
 
                 ))}
