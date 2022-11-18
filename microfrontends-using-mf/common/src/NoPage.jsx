2d3009fa690e1ef87eed61ca08234def42eae588
Sadhan Sarker <cse.sadhan@gmail.com>
2022-11-19 01:45:31 +0600

    updated no page content

--- microfrontends-using-mf/common/src/NoPage.jsx
+++ microfrontends-using-mf/common/src/NoPage.jsx
@@ -3,17 +3,8 @@ import React from "react";
 export default () => {
   
   return (
-    <header className="bg-orange-700 text-white font-bold text-3xl p-5 flex">
-      <a href="/" className="flex-grow"> Coacktail </a>
-      <div>
-
-        {/* <button
-          onClick={cartPanelToggle}
-          className="bg-indigo-800 text-white font-bold py-2 px-4 rounded">
-          Cart Panel {isShowCartPanel? 'true' : 'false'}
-        </button> */}
-
-      </div>
-    </header>
+    <div className="">
+        <h1 className="text-3xl font-bold">No Page</h1>
+    </div>
   );
 };
