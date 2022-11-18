25c57235fae2ce4bbbf17715f1fc2a3c98e24b6a
Sadhan Sarker <cse.sadhan@gmail.com>
2022-11-19 01:50:22 +0600

    home view statistic view changed

--- microfrontends-using-mf/common/src/pages/Home.jsx
+++ microfrontends-using-mf/common/src/pages/Home.jsx
@@ -8,13 +8,18 @@ const Home = () => {
 
     const {userId, createUserIdentity} = useViewingStatisticsStore();
 
-    return (<div >
+    return (<div className="p-5">
         
         <h1 className="text-3xl font-bold">Hello from React and Module Federation</h1>
         <h1 className="text-3xl font-bold">React Version 18</h1>
         <h1 className="text-3xl font-bold">User ID: {userId}</h1>
 
-        <Link to={'/statistics'}>Show more statistics</Link>
+
+        <div className="w-full py-3">
+            <div className="inline-block mr-2 mt-2">
+            <Link to={'/statistics'} className="focus:outline-none text-orange-600 text-sm py-2.5 px-5 rounded-md border border-orange-600 hover:bg-yellow-50 items-center mt-100">View Statistics</Link>               
+            </div>
+        </div>
         
     </div>)
 }
