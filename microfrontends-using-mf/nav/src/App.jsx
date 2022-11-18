f7fa3f09af4fcc88eff1d694c5be1f7ba58f5530
Sadhan Sarker <cse.sadhan@gmail.com>
2022-11-18 20:08:45 +0600

    created wf nav react app

---
+++ microfrontends-using-mf/nav/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {createRoot} from 'react-dom/client';
+
+import "./index.scss";
+
+
+const App = () => {
+
+  return (
+
+    <div>
+      <h1 className="text-3xl font-bold">Hello from React and Module Federation</h1>
+      <h1 className="text-3xl font-bold">React Version 18</h1>
+    </div>
+
+  );
+
+};
+
+const rootElement = document.getElementById('app');
+const root = createRoot(rootElement);
+root.render(<App />);
