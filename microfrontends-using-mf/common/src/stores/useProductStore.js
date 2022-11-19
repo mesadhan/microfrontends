ab66d424ff9132b5dd31abff548c2b9b7c16b526
Sadhan Sarker <cse.sadhan@gmail.com>
2022-11-19 23:15:01 +0600

    product store updated with caching approach

--- microfrontends-using-mf/common/src/stores/useProductStore.js
+++ microfrontends-using-mf/common/src/stores/useProductStore.js
@@ -1,79 +1,83 @@
 import create from "zustand";
-import { devtools } from 'zustand/middleware'
+import {devtools, persist} from 'zustand/middleware'
 
+const BASE_API_URL = 'https://www.thecocktaildb.com/api/json/v1/1';
 
+const useProductStore = create(
+    persist((set, get) => ({
 
-const useProductStore = create((set) => ({
-    
-    products: [],
-    categories: [],
-    ingredients: [],
-    glasses: [],
-    productDetails: [],
-    
-    productDetailById: async (id) => {
-      const apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
-      const res = await fetch(apiUrl);
-      const data = await res.json();
+      products: [],
+      categories: [],
+      ingredients: [],
+      glasses: [],
+      productDetails: [],
 
-      console.log('asdfsafsafasfsda', data?.drinks);
+      productDetailById: async (id) => {
+        const apiUrl = `${BASE_API_URL}/lookup.php?i=${id}`;
+        const res = await fetch(apiUrl);
+        const data = await res.json();
 
-      return set((state) => ({ productDetails: data?.drinks }));
-    },
-    fetchCategories: async () => {
-      const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
-      const res = await fetch(apiUrl);
-      const data = await res.json();
-      set((state) => ({ categories: data?.drinks }));
-    },
-    fetchIngredients: async () => {
-      const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-      const res = await fetch(apiUrl);
-      const data = await res.json();
-      set((state) => ({ ingredients: data?.drinks }));
-    },
-    fetchGlasses: async () => {
-      const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list'
-      const res = await fetch(apiUrl);
-      const data = await res.json();
-      set((state) => ({ glasses: data?.drinks }));
-    },
+        return set((state) => ({productDetails: data?.drinks}));
+      },
+      fetchCategories: async () => {
+        if(get().categories.length > 0) return;
+        const apiUrl = `${BASE_API_URL}/list.php?c=list`
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        set((state) => ({categories: data?.drinks}));
+      },
+      fetchIngredients: async () => {
+        if(get().ingredients.length > 0) return;
+        const apiUrl = `${BASE_API_URL}/list.php?i=list`
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        set((state) => ({ingredients: data?.drinks}));
+      },
+      fetchGlasses: async () => {
+        if(get().glasses.length > 0) return;
+        const apiUrl = `${BASE_API_URL}/list.php?g=list`
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        set((state) => ({glasses: data?.drinks}));
+      },
 
-    getFilteredList: async(filterName, filterValue, query) => {
+      getFilteredList: async (filterName, filterValue, query) => {
 
-      console.log('filterName', filterName, 'filterValue', filterValue, 'query', query);
-       
-      let apiUrl = ''
-      let productsTemp = []
+        console.log('message', {filterName, filterValue, query});
 
-      const config = {
-        'category': 'c',
-        'ingredient': 'i',
-        'glass': 'g'
-      }
+        let apiUrl = ''
+        let productsTemp = []
 
-      if(filterName && !query) {
-        apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?${config[filterName]}=${filterValue}`;
-      }
-      else if(query) {
-        apiUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`;
-      }
-    
+        const config = {
+          'category': 'c',
+          'ingredient': 'i',
+          'glass': 'g'
+        }
 
-      try {
+        if (filterName && !query) {
+          apiUrl = BASE_API_URL+`/filter.php?${config[filterName]}=${filterValue}`;
+        } else if (query) {
+          apiUrl = BASE_API_URL+`/search.php?s=${query}`;
+        }
+
+        try {
           const res = await fetch(apiUrl);
-          if(res){
+          if (res) {
             const data = await res.json();
-            productsTemp = [...new Set(productsTemp.concat(...data?.drinks))];  
+            productsTemp = [...new Set(productsTemp.concat(...data?.drinks))];
           }
-      }catch (error) {
-        console.log('error', error.message);
-      }
+        } catch (error) {
+          console.log('error', error.message);
+        }
 
-      return set((state) => ({ products: productsTemp }));
-      
-    }
-    
-}));
+        return set((state) => ({products: productsTemp}));
+
+      },
+
+    }), {
+      name: 'product-store', // unique name
+      getStorage: () => sessionStorage,
+    })
+);
 
 export default useProductStore;
